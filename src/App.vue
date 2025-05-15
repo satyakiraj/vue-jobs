8e76f3a02d720feb25e213f7cc2a45d662950e28
satyakiraj <satyakithecoder@gmail.com>
2025-05-15 13:25:11 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AppNavbar from '@/components/AppNavbar.vue'
+</script>
+
+<template>
+  <header class="text-red-600 text-3xl font-sans font-semibold"></header>
+ <AppNavbar />
+</template>
+
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,8 @@
+<script setup>
+import logo from '../assets/img/logo.png'
+</script>
+
+<template>
+<img :src="logo" alt="logo" class="logo">
+</template>
+
