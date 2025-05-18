f51d8685884954efc7b6604355d71c3bd696988c
satyakiraj <satyakithecoder@gmail.com>
2025-05-18 22:31:21 +0530

    issue#2

--- src/components/hero.vue
+++ src/components/hero.vue
@@ -3,14 +3,14 @@
 <template>
   <!--  issue#2-->
   <!--  fix the overlapping issue in smaller screens-->
-  <div
-    class="bg-green-600 w-full h-[15vh] absolute bottom-0">
+  <div class="bg-green-600 w-full h-[15vh] absolute bottom-0">
     <div
-      class="flex flex-col items-center justify-center space-y-2 absolute bottom-0 h-[10vh] w-screen">
+      class="flex flex-col items-center justify-center space-y-2 absolute bottom-0 h-[10vh] w-screen"
+    >
       <h1 class="font-bold fluid-text text-center mb-2 text-white">Become a Vue Dev</h1>
-      <p class="text-white text-nowrap text-[20px] font-normal">Find the Vue job that fits your
-        skills
-        and needs</p>
+      <p class="text-white text-nowrap text-[20px] font-normal">
+        Find the Vue job that fits your skills and needs
+      </p>
     </div>
   </div>
 </template>
