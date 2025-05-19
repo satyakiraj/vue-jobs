a8f035065eb3b0010e3f50d0ed56bf7f9825cd4f
satyakiraj <satyakithecoder@gmail.com>
2025-05-19 15:39:03 +0530

    issue#3

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Navbar from './components/navbar.vue'
 import Hero from './components/hero.vue'
+import Listings from './components/listings.vue'
 </script>
 
 <template>
   <Navbar/>
   <Hero/>
+  <Listings/>
 </template>
---
+++ src/components/listings.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+  <!--Add the job for hiring and list jobs option-->
+</template>
