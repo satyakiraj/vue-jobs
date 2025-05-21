8cfe62ec8bfb03f886516c9ae1315d519597a65d
satyakiraj <satyakithecoder@gmail.com>
2025-05-21 15:26:36 +0530

    job listings

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import Navbar from '@/components/navbar.vue'
 import Hero from '@/components/hero.vue'
 import HomeCards from '@/components/homeCards.vue'
+import JobListings from '@/components/JobListings.vue'
 </script>
 
 <template>
   <Navbar/>
   <Hero/>
   <HomeCards/>
+  <JobListings/>
 </template>
--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,15 +1,18 @@
 <script setup>
-import Job from '@/components/jobs.vue'
 import {ref} from 'vue'
+import Job from '@/components/jobs.vue'
 import jobData from '@/jobs.json'
 
-const jobs = ref(jobData)
+const jobs = ref(jobData.jobs)
 </script>
 
 <template>
-  <div class="h-auto mx-4 px-4 py-10 bg-green-300 rounded-md">
-    <h1 class="font-bold text-center">Browse Jobs</h1>
-    <div class="grid gap-6 grid-cols-1 md:grid-cols-3 bg-transparent w-auto h-auto">
+  <div class="h-auto mx-4 px-4 py-10 bg-green-100 rounded-md">
+    <h1 class="font-bold text-center text-3xl text-green-500">Browse Jobs</h1>
+
+    <div
+      class="flex flex-col space-y-6 items-center justify-center 2xl:grid gap-4 grid-cols-3 bg-transparent box-content h-auto px-3"
+    >
       <Job v-for="job in jobs" :key="job.id" :job="job"/>
     </div>
   </div>
