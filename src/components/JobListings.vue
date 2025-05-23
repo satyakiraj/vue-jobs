bef2b145ed08fae906e37441b3dd07329c4369ee
satyakiraj <satyakithecoder@gmail.com>
2025-05-23 14:05:18 +0530

    App.vue changed

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,9 +1,15 @@
 <script setup>
-import {ref} from 'vue'
+import {ref, defineProps} from 'vue'
 import Job from '@/components/jobs.vue'
 import jobData from '@/jobs.json'
-
 const jobs = ref(jobData.jobs)
+defineProps({
+  limit: Number,
+  showButton:{
+    type: Boolean,
+    default: false,
+  }
+})
 </script>
 
 <template>
@@ -13,7 +19,13 @@ const jobs = ref(jobData.jobs)
     <div
       class="flex flex-col space-y-6 items-center justify-center 2xl:grid gap-4 grid-cols-3 bg-transparent box-content h-auto px-3"
     >
-      <Job v-for="job in jobs" :key="job.id" :job="job"/>
+      <Job v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job"/>
     </div>
   </div>
+  <section class="w-screen h-auto my-5 flex justify-center py-5">
+    <a
+      class="bg-black text-white w-1/2 text-center py-4 cursor-pointer h-14 tex-xl font-medium rounded-2xl hover:bg-gray-800" v-if="showButton">View
+      All
+      Jobs</a>
+  </section>
 </template>
