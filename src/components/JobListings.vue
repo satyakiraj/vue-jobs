da2b6770ab9e99c3d795e538253f86250b40ac2b
satyakiraj <satyakithecoder@gmail.com>
2025-05-24 21:28:54 +0530

    router

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,14 +1,15 @@
 <script setup>
-import {ref, defineProps} from 'vue'
+import { defineProps, ref } from 'vue'
 import Job from '@/components/jobs.vue'
 import jobData from '@/jobs.json'
+
 const jobs = ref(jobData.jobs)
 defineProps({
   limit: Number,
-  showButton:{
+  showButton: {
     type: Boolean,
     default: false,
-  }
+  },
 })
 </script>
 
@@ -16,16 +17,15 @@ defineProps({
   <div class="h-auto mx-4 px-4 py-10 bg-green-100 rounded-md">
     <h1 class="font-bold text-center text-3xl text-green-500">Browse Jobs</h1>
 
-    <div
-      class="flex flex-col space-y-6 items-center justify-center 2xl:grid gap-4 grid-cols-3 bg-transparent box-content h-auto px-3"
-    >
-      <Job v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job"/>
+    <div class="grid gap-6 grid-cols-1 2xl:grid-cols-3 justify-center px-3 bg-transparent">
+      <Job v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
     </div>
   </div>
   <section class="w-screen h-auto my-5 flex justify-center py-5">
     <a
-      class="bg-black text-white w-1/2 text-center py-4 cursor-pointer h-14 tex-xl font-medium rounded-2xl hover:bg-gray-800" v-if="showButton">View
-      All
-      Jobs</a>
+      v-if="showButton"
+      class="bg-black text-white w-1/2 text-center py-4 cursor-pointer h-14 tex-xl font-medium rounded-2xl hover:bg-gray-800"
+      >View All Jobs</a
+    >
   </section>
 </template>
